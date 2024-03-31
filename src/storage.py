a4aea09794581c76c5b25140e5f3764848826905
meshya <mohali4h@gmail.com>
2024-03-31 06:10:29 +0330

    write torage scripts

--- src/storage.py
+++ src/storage.py
@@ -1,20 +1,68 @@
 import domain
-def read():
-    d = domain.server(
-        type='dns',
-        url="1.1.1.1"
-    )
-    c = domain.server(
-        type='dns',
-        url="8.8.8.8"
-    )
+import settings
+import report
+import os
+import json
 
-    return domain.appData(
-        [
-            domain.profile(
-                server1 = d,
-                server2 = c,
-                name = "shekan",
+def read():
+    def loadServer(rawServ)->domain.server:
+        return domain.server(
+            **rawServ
+        )
+    emptyConf = domain.appData([])
+    if not os.path.exists(settings.APP_DATA_FILE):
+        report.DataFile.InvalidFile()
+        return emptyConf
+    with open(settings.APP_DATA_FILE,"r") as f:
+        try:
+            rawData = json.loads(
+                f.read()
             )
+        except:
+            report.DataFile.InvalidFile()
+            return emptyConf
+        profiles = []
+        for rawProf in  rawData.get('profiles',[]):
+            try:
+                profWargs = {
+                    "server1" : loadServer(rawProf['server1']),
+                    "name": rawProf['name']
+                }
+                if "server2" in rawProf:
+                    profWargs['server2'] = loadServer(rawProf['server2'])
+                profiles.append(
+                    domain.profile(**profWargs)
+                )
+            except:
+                report.DataFile.InvalidProfile()
+        return domain.appData(
+            profiles
+        )
+
+def write(appData:domain.appData):
+    def dumpServ(serv:domain.server) -> dict:
+        return {
+            'type' : serv.type,
+            'url' : serv.url
+        }
+    def dumpProf(prof:domain.profile) -> dict:
+        rawProf = {
+            "server1":dumpServ(prof.server1),
+            "name":prof.name
+        }
+        if prof.server2:
+            rawProf['server2'] = dumpServ(prof.server2)
+        return rawProf
+    rawData = {
+        "profiles":[
+            dumpProf(p) for p in appData.profiles
         ]
-    )
+    }
+    try:
+        with open(settings.APP_DATA_FILE,"w+") as f:
+            f.write(
+                json.dumps(rawData)
+            )
+    except:
+        report.DataFile.InvalidFile()
+    
